e7105cdd487802c83b6b453424b91938e003aff9
Marcus-Oliveira <marcusviniciusdantasoliveira@gmail.com>
2016-08-15 09:06:22 -0300

    Config

--- build_structure.php
+++ build_structure.php
@@ -1,11 +1,71 @@
 <?php
 
+include_once ("config.php");
+
 $myfile = fopen("app_structure.json", "r") or die("Unable to open file!");
 $decodedFile = json_decode(fread($myfile,filesize("app_structure.json")), true);
 fclose($myfile);
 
-echo "Structure<pre>";
-var_dump($decodedFile["classes"]);
-echo "</pre>";
+//echo "Structure<pre>";
+//var_dump($decodedFile["classes"]);
+//echo "</pre>";
+
+foreach($decodedFile["classes"] AS $key => $folders){
+
+    /**
+    * Procurando e criando pastas raiz do projeto]
+    * Exemplo /app
+    *
+    **/    
+    
+    foreach($folders AS $key => $folder){
+        $pathname = $key;
+        make_path($pathname);
+        
+        //Criando as subpastas        
+        foreach($folders[$key] AS $key2 => $value){
+
+            if($key2 === "folder"){
+                
+                foreach($folders[$key][$key2] AS $key3 => $value){
+//                    echo "<pre>";
+//                    var_dump($key3);
+//                    var_dump($value);
+//                    echo "</pre>";
+                    
+                    $pathname .= DS . $key3;
+//                    var_dump($pathname);
+                    make_path($pathname);
+                    $pathname = str_replace( DS . $key3, "", $pathname);
+                    
+                    //Criando arquivos e funções das pastas
+                    foreach($folders[$key][$key2][$key3] AS $key4 => $value){
+                        echo "<pre>";
+                        var_dump($key4);
+                        var_dump($value);
+                        echo "</pre>";
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+    
+//    if($key === "folder"){
+//        echo "<pre>";
+////        var_dump($key);
+//        
+//        echo "</pre>";
+//    }
+    
+    
+//    foreach($class["folder"] AS $key->$folder){
+//        echo "$folder<br/>";
+//    }
+}
 
 ?>
--- config.php
+++ config.php
@@ -4,6 +4,40 @@
 
 require __DIR__.'/vendor/autoload.php';
 
+define('DS', DIRECTORY_SEPARATOR);
 
+/**
+ * Create  Directory Tree if Not Exists
+ * <br/>If you are passing a path with a filename on the end, pass true as the second parameter to snip it off
+ * @param string $pathname
+ * @param bool $is_filename
+ * @return boolean
+ */
+function make_path($pathname, $is_filename = false) {
+    if ($is_filename) {
+        $pathname = substr($pathname, 0, strrpos($pathname, '/'));
+    }
+    // Check if directory already exists
+    if (is_dir($pathname) || empty($pathname)) {
+        return true;
+    }
+
+    // Ensure a file does not already exist with the same name
+    $pathname = str_replace(array('/', '\\'), DS, $pathname);
+    if (is_file($pathname)) {
+        trigger_error('mkdirr() File exists', E_USER_WARNING);
+        return false;
+    }
+    // Crawl up the directory tree
+    $next_pathname = substr($pathname, 0, strrpos($pathname, DS));
+    if (make_path($next_pathname)) {
+        if (!file_exists($pathname)) {
+            $result = mkdir($pathname);
+            chmod($pathname, 0775);
+            return $result;
+        }
+    }
+    return false;
+}
 
 ?>
